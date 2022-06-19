393b7d613ca21c7826b2c197031de910e2351183
Aiden Bai 白宇彤 <aiden.bai05@gmail.com>
2022-06-19 16:08:00 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import reactLogo from '/react.svg';
+import millionLogo from '/million.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://millionjs.org" target="_blank">
+          <img src={millionLogo} className="logo" alt="Million logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo" alt="React logo" />
+        </a>
+      </div>
+      <h1>Million + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Million and React logos to learn more
+      </p>
+      <p>
+        <a href="https://github.com/aidenybai/million-react">
+          Fork the GitHub repo
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
